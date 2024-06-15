a0033bd366e9f722a5d27fa6a0d3347b36ba9cc4
eric.crowell <eric.crowell@ecrow.dev>
2024-06-15 15:59:58 +0200

    dev: removed web aliases

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,28 +1,4 @@
 import webpack from 'webpack';
-import { join, dirname } from 'node:path';
-import { fileURLToPath } from 'node:url';
-import fs from 'node:fs';
-
-/**
- * Create aliases for all packages that have a `src` folder.
- */
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-const packagesPath = join(__dirname, '../../packages');
-const packages = fs.readdirSync(packagesPath)
-  .filter((file) => fs.statSync(join(packagesPath, file)).isDirectory());
-
-const aliases = packages.reduce((acc, pack) => {
-  const srcFolder = join(packagesPath, pack, 'src');
-  const hasSrc = fs.existsSync(srcFolder);
-  if (!hasSrc) {
-    return acc;
-  }
-
-  acc[`@-/${pack}$`] = packagesPath + '/' + pack + '/src';
-
-  return acc;
-}, {});
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -35,12 +11,6 @@ const nextConfig = {
         resourceRegExp: /^node:stream$|^pg-native$|^cloudflare:sockets$/,
       }),
     );
-
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      ...aliases,
-    };
-    
     return config;
   }
 };
