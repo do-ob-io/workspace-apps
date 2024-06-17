3b74e7a6f86a2bcea4268f06458320df0001198c
eric.crowell <eric.crowell@ecrow.dev>
2024-06-17 17:17:14 +0200

    dev: removed dead code in tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,15 +1,6 @@
 import type { Config } from 'tailwindcss';
-import path from 'node:path';
-import fs from 'node:fs';
 import { nextui }  from '@nextui-org/react';
 import tailwindTypography from '@tailwindcss/typography';
-
-const nextuiContent = path.join(
-  fs.realpathSync('./node_modules/@nextui-org/theme'),
-  'dist/**/*.{js,ts,jsx,tsx}'
-);
-
-console.log(nextuiContent);
 
 const config: Config = {
   darkMode: 'class',
